1ff05f2fc42f19a5cc1587cdcf35973ac94198c9
Darren Gardner <jamerican10@hotmail.comgit config --global user.name DGWonKanobi>
2023-10-05 00:47:40 -0400

    positivearray-negativearray-detecthit function added

--- app.js
+++ app.js
@@ -6,58 +6,180 @@ const username = prompt("Enter your username:");
 const negatives = ["Negativity", "Complaining", "Gossip", "Stress"];
 const positives = ["Gratitude", "Abundance", "Health", "Positivity"];
 
-// Countdown timer
-// ...
+// // GLOBAL DOM / VARIABLES
+// const mushroom = document.querySelector('#mush');
+// const movement = document.querySelector('#movement');
+// const game = document.querySelector('#game');
+// const score = document.querySelector('#score');
+// const status = document.querySelector('#status');
+// const ctx = game.getContext('2d');
+// let shrek;
+// let donkey;
 
-// Game instructions
-// ...
+// ====================== PAINT INTIAL SCREEN ======================= //
+// EVENT LISTENERS
+window.addEventListener('DOMContentLoaded', function () {
+    //load the donkey on the shrek on page 
+    shrek = new Crawler(100, 200, '#bad55', 50, 100);
+    donkey = new Crawler(10, 20, 'brown', 25, 25);
 
-// Character movement
-document.addEventListener("keydown", moveCharacter);
+    let runGame = setInterval(gameLoop, 60);
+})
+document.addEventListener('keydown', movementHandler);
 
-// Falling objects
-setInterval(createFallingObject, 1000); // Adjust timing as needed in milliseconds
+// ====================== SETUP FOR CANVAS RENDERING ======================= //
+// 2D rendering context for canvas element
+// This is used for drawing shapes, text, images, etc.
+game.setAttribute('height', getComputedStyle(game)['height']);
+game.setAttribute('width', getComputedStyle(game)['width']);
 
-// Collision detection
-function detectCollision(character, object) {
-  // ...
+// ====================== ENTITIES ======================= //
+class Crawler {
+    constructor(x, y, color, width, height) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        this.alive = true;
 
-  if (collisionType === "positive") {
-    increaseLifeScore();
-  } else if (collisionType === "negative") {
-    decreaseLifeScore();
-  }
+
+        this.render = function () {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
+}
+// let testCrawler = new Crawler(150, 20, 'blue', 200, 200);
+// testCrawler.render();
+
+
+// KEYBOARD LOGIC
+
+function movementHandler(e) {
+
+    // make a conditional for each direction
+    if (e.key === 'w' || e.key === 'ArrowUp') {
+        donkey.y - 10 >= 0 ? (donkey.y -= 10) : null;
+    } else if (e.key === 's' || e.key === 'ArrowDown') {
+        donkey.y + 10 <= game.height - donkey.height ? (donkey.y += 10) : null;
+    } else if (e.key === 'a' || e.key === 'ArrowLeft') {
+        donkey.x - 10 >= 0 ? (donkey.x -= 10) : null;
+    } else if (e.key === 'd' || e.key === 'ArrowRight') {
+        donkey.x + 10 <= game.width - donkey.width ? (donkey.x += 10) : null;
+    }
+
 }
 
-// Score tracking
-function updateLifeScore() {
-  // ...
 
-  // Determine user icon based on life score
-  // ...
+// function shrekAIMovement() {
+//     // make a conditional for each direction
+//     // first move the shrek up, then move to the right, then to the left, the down and repeat
+//     console.log('Shrek AI');
+//     moveUp();
+//     moveUp();
 
-  // Check win condition
-  if (lifeScore >= highestAssignedValue) {
-    showWinScreen();
-  }
+//     setTimeout(() => {
+//         moveRight();
+//         moveRight();
+//     }, 500);
+
+//     setTimeout(() => {
+//         moveDown();
+//         moveDown();
+//     }, 1000);
+
+// }
+
+// function moveUp() {
+//     if (shrek.y - 10 >= 0) {
+//         shrek.y -= 10;
+//     } else {
+//         shrek.y = 100
+//     }
+// }
+
+// function moveRight() {
+//     if (shrek.x + 10 <= game.width - shrek.width) {
+//         shrek.x += 10;
+//     } else {
+//         shrek.x = 100;
+//     }
+// }
+
+// function moveDown() {
+//     if (shrek.y + 10 <= game.height - shrek.height) {
+//         shrek.y += 10
+//     } else {
+//         shrek.y = 100;
+//     }
+// }
+
+// function moveLeft() {
+//     if (shrek.x - 10 >= 10) {
+//         shrek.x -= 10
+//     } else {
+//         shrek.x = 100;
+//     }
+// }
+
+
+
+
+// ====================== HELPER FUNCTIONS ======================= //
+function addNewShrek() {
+    shrek.alive = false;
+    //use setTime function createa new Shrek after1 second ( 1000 milliseconds)
+    setTimeout(function () {
+        let randomX = Math.floor(Math.random() * game.width - 40);
+        let randomY = Math.floor(Math.random() * game.width - 80)
+        // random color
+        const colors = ['pink', 'purple', 'red', 'cyan', 'magenta', 'teal', 'green'];
+        let randomIndex = Math.floor(Math.random() * colors.length - 1);
+        let randomColor = colors[randomIndex];
+        // create new Shrek
+        shrek = new Crawler(randomX, randomY, randomColor, 50, 100);
+    }, 1000);
+    return true;
 }
 
-// Game win condition
-function showWinScreen() {
-  // ...
+// ====================== GAME PROCESSES ======================= //
+function gameLoop() {
+    // clear the canvas
+    ctx.clearRect(0, 0, game.width, game.height)
+    //display x and y coordinates for our donkey
+    movement.textContent = `X: ${donkey.x}\nY: ${donkey.y}`;
 
-  // Display Goku in Ultra Instinct and a congratulatory message
+    // check to see if shrek is alive
+    if (shrek.alive) {
+        // render the shrek
+        shrek.render();
+        // check for collision between donkey and shrek
+        let hit = detectHit(donkey, shrek);
+
+
+    }
+    //render the donkey
+    donkey.render();
 }
+// ====================== COLLISION DETECTION ======================= //
 
-// Game over condition
-function showGameOverScreen() {
-  // ...
-
-  // Allow player to restart the game
+function detectHit(player, opp) {
+    let hitTest = (
+        player.y + player.height > opp.y && //
+        player.y < opp.y + opp.height &&
+        player.x + player.width > opp.x && //
+        player.x < opp.x + opp.width
+    );
+    console.log(hitTest);
+    if (hitTest) {
+        //add 100 points to current score
+        let newScore = Number(score.textContent) + 100;
+        console.log(score.textContent);
+        score.textContent = newScore;
+        // return a new Shrek with the addNewShrek function
+        return addNewShrek();
+    } else {
+        return false;
+    }
 }
-
-// Sound and graphics
-// ...
-
-// Event handling (e.g., restart button)
-// ...
